cb9be96b80ef174f40fbd7c4a221daf5e4c3a91a
shadowninja71 <viktor.hvidpersson@elev.ga.ntig.se>
2024-10-02 09:58:37 +0200

    fick brawl att funka

--- brawl.py
+++ brawl.py
@@ -1,23 +1,31 @@
 from random import randint
 
-input('Enter your name:')
-x = input()
+player_one_name = input('Enter your name:') 
 
 player_one_life= 10
 player_two_life = 10
 
-player_one_role = randint(1, 6)
-player_two_role = randint(1, 6)
+round = 0
 
-if player_one_role > player_two_role:
-        print(f"Spelare ett vinner med tärningskastet: {player_one_role}")
-        player_two_life -= 1
-elif player_two_role > player_one_role:
-        print(f"Spelare två vinner med tärningskastet: {player_two_role}")
-        player_one_life -= 1
-else:
-        print(f"ingen spelare vinner, det är oavgjort med tärningstalet: {player_one_role}")
-if player_one_life == 0:
-        print(f"spelare två van med {player_two_life} kvar")
-elif player_two_life == 0:
-        print(f"spelare ett van med {player_one_life} kvar")
+while player_one_life > 0 and player_two_life > 0:
+
+        player_one_role = randint(1, 6)
+        player_two_role = randint(1, 6)
+
+        if player_one_role > player_two_role:
+                print(f"{player_one_name} van med tärningskastet: {player_one_role}")
+                player_two_life -= 1
+                round += 1
+        elif player_two_role > player_one_role:
+                print(f"Spelare två vinner med tärningskastet: {player_two_role}")
+                player_one_life -= 1
+                round += 1
+        else:
+                print(f"ingen spelare vinner, det är oavgjort med tärningstalet: {player_one_role}")
+                round += 1
+        if player_one_life == 0:
+                print(f"spelare två van med {player_two_life} liv kvar")
+                print(f"och det tog {round} runder för spelare twå att vina")
+        elif player_two_life == 0:
+                print(f"{player_one_name} van med {player_one_life} liv kvar")
+                print(f"och det tog {round} runder för {player_one_name} att vina")
