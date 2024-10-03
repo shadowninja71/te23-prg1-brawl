4a0a1d2bd303dd9cc4364d1cd2a3f797815e7fad
shadowninja71 <viktor.hvidpersson@elev.ga.ntig.se>
2024-10-03 09:41:02 +0200

    gjorde krit

--- brawl.py
+++ brawl.py
@@ -1,8 +1,8 @@
 from random import randint
 
-player_one_name = input('Enter your name:') 
+player_one_name = input('skriv in dit namn:') 
 
-player_one_life= 10
+player_one_life = 10
 player_two_life = 10
 
 round = 0
@@ -13,13 +13,23 @@ while player_one_life > 0 and player_two_life > 0:
         player_two_role = randint(1, 6)
 
         if player_one_role > player_two_role:
-                print(f"{player_one_name} van med tärningskastet: {player_one_role}")
-                player_two_life -= 1
-                round += 1
+                if player_one_role == 6:
+                    print(f"{player_one_name} rulade en 6")
+                    player_two_life -= 2
+                    round += 1
+                else:
+                    print(f"{player_one_name} van med tärningskastet: {player_one_role}")
+                    player_two_life -= 1
+                    round += 1
         elif player_two_role > player_one_role:
-                print(f"Spelare två vinner med tärningskastet: {player_two_role}")
-                player_one_life -= 1
-                round += 1
+                if player_two_role == 6:
+                    print(f"spelare två rulade en 6")
+                    player_one_life -= 2
+                    round +=1
+                else:
+                    print(f"Spelare två vinner med tärningskastet: {player_two_role}")
+                    player_one_life -= 1
+                    round += 1
         else:
                 print(f"ingen spelare vinner, det är oavgjort med tärningstalet: {player_one_role}")
                 round += 1
