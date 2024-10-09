f49277be0c91445a7e2fc1cbf5b8b028dfa1cc74
shadowninja71 <viktor.hvidpersson@elev.ga.ntig.se>
2024-10-09 09:58:16 +0200

    brawel klar

--- brawl.py
+++ brawl.py
@@ -17,19 +17,23 @@ while player_one_life > 0 and player_two_life > 0:
                     print(f"{player_one_name} rulade en 6")
                     player_two_life -= 2
                     round += 1
+                    input("enter:")
                 else:
                     print(f"{player_one_name} van med tärningskastet: {player_one_role}")
                     player_two_life -= 1
                     round += 1
+                    input("enter:")
         elif player_two_role > player_one_role:
                 if player_two_role == 6:
                     print(f"spelare två rulade en 6")
                     player_one_life -= 2
                     round +=1
+                    input("enter:")
                 else:
                     print(f"Spelare två vinner med tärningskastet: {player_two_role}")
                     player_one_life -= 1
                     round += 1
+                    input("enter:")
         else:
                 print(f"ingen spelare vinner, det är oavgjort med tärningstalet: {player_one_role}")
                 round += 1
