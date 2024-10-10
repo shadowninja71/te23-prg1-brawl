920032405c85e665b9935da9e03d55e4962ba7e5
shadowninja71 <viktor.hvidpersson@elev.ga.ntig.se>
2024-10-10 09:46:20 +0200

    klar

--- brawl.py
+++ brawl.py
@@ -14,10 +14,15 @@ while player_one_life > 0 and player_two_life > 0:
 
         if player_one_role > player_two_role:
                 if player_one_role == 6:
-                    print(f"{player_one_name} rulade en 6")
+                    print(f"{player_one_name} rulade en 6 och gjorde dubelt så mycket skada på spelare två")
                     player_two_life -= 2
                     round += 1
                     input("enter:")
+                elif player_one_role == 1:
+                    print(f"{player_one_name} rulade en 1 och misade")
+                    round += 1
+                    input("enter:")
+
                 else:
                     print(f"{player_one_name} van med tärningskastet: {player_one_role}")
                     player_two_life -= 1
@@ -25,18 +30,23 @@ while player_one_life > 0 and player_two_life > 0:
                     input("enter:")
         elif player_two_role > player_one_role:
                 if player_two_role == 6:
-                    print(f"spelare två rulade en 6")
+                    print(f"spelare två rulade en 6 och gjorde dubelt så mycket skada på {player_one_name}")
                     player_one_life -= 2
                     round +=1
                     input("enter:")
+                elif player_one_role == 1:
+                    print(f"spelare två rulade en 1 och misade")
+                    round += 1
+                    input("enter:")
                 else:
-                    print(f"Spelare två vinner med tärningskastet: {player_two_role}")
+                    print(f"Spelare två van med tärningskastet: {player_two_role}")
                     player_one_life -= 1
                     round += 1
                     input("enter:")
         else:
-                print(f"ingen spelare vinner, det är oavgjort med tärningstalet: {player_one_role}")
+                print(f"ingen spelare van, det är oavgjort med tärningstalet: {player_one_role}")
                 round += 1
+                input("enter")
         if player_one_life == 0:
                 print(f"spelare två van med {player_two_life} liv kvar")
                 print(f"och det tog {round} runder för spelare twå att vina")
